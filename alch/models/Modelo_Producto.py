0f41c582cce2f8dc3904b896953a450a56360213
RicardoSanPer <ricardosp@ciencias.unam.mx>
2024-05-26 13:32:15 -0600

    consultar producto

--- alch/models/Modelo_Producto.py
+++ alch/models/Modelo_Producto.py
@@ -1,8 +1,17 @@
 from sqlalchemy import delete
 from alch.alchemyClasses.producto import Producto
 from alch.alchemyClasses import db
+
 class ModeloProducto():
     def agregar(self, producto):
         db.session.add(producto)
         db.session.commit()
         return True
+    
+    def consultar_producto(id_producto):
+        data = None
+        try:
+            data = Producto.query.filter_by(id_producto = id_producto).first()
+        except Exception as e:
+            print(e)
+        return data
